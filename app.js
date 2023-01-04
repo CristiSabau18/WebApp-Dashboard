b44970837cf95c6e8a458e0e89856e3629263080
CristiSabau18 <cristisabau18@yahoo.com>
2023-01-04 16:58:26 +0200

    updated app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const alertBanner = document.getElementById('alert');
 
-alertBanner.innerHTML = `<p><strong>Alert:</strong> You have unread messages</p><p class="alert-banner-close">x</p>`;
+alertBanner.innerHTML = `<p><strong>Alert:</strong> You have 6 overdue tasks</p><p class="alert-banner-close">x</p>`;
 
 alertBanner.addEventListener('click', e => {
     const element = e.target;
